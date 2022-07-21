57f3e223a7801ada3a8fee5af7c9255f474ac498
Roman Maximov <manul99991@mail.ru>
2022-07-22 00:12:04 +0300

    homework

---
+++ HomeWork8/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+/*Console.WriteLine("Введите количество строк:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int n = Convert.ToInt32(Console.ReadLine());;
+int[,] firstMassive = new int [m, n];
+
+
+FillArray(firstMassive);
+PrintArray(firstMassive);
+Console.WriteLine();
+OrderedArray(firstMassive);
+Console.WriteLine("Отсортированный массив: \n");
+PrintArray(firstMassive);
+
+
+
+void OrderedArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] > array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;          
+        }
+      }
+    }
+  }
+}
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            firstMassive[i, j] = new Random().Next(0, 10); 
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}*/
