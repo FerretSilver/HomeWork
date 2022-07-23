8beab0a1323f344d27cf6e7fabd1b7c403650498
Roman Maximov <manul99991@mail.ru>
2022-07-23 18:14:26 +0300

    HomeWork

--- HomeWork8/Program.cs
+++ HomeWork8/Program.cs
@@ -195,3 +195,46 @@ void FillArray(int[,] array)
     }
   }
 }*/
+
+
+//Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет
+//построчно выводить массив, добавляя индексы каждого элемента.
+//массив размером 2 x 2 x 1
+
+
+
+/*int[,,] myArray = new int[2, 1, 2];
+
+FillArray(myArray);
+PrintArray(myArray);
+
+void FillArray(int[,,] array)
+{
+  
+  for (int i = 0; i < array.GetLength(0); i++)
+  {    
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(2); k++)
+      {
+        array[i, j, k] = new Random().Next(10 , 99);
+      }
+    }
+  }
+  
+}
+void PrintArray(int[,,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {    
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(2); k++)
+      {
+        Console.Write($"{array[i, j, k]} ({i},{j},{k}) ");
+      }
+      Console.WriteLine();
+    }       
+  }
+    Console.WriteLine();
+}*/
